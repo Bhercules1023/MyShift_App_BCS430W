5952fd2263d8a5ab27063429e64102ffd599bf64
Bhercules1023 <hercb@farmingdale.edu>
2025-06-11 17:26:47 -0400

    Add files via upload

---
+++ src/main/java/Employee.java
@@ -0,0 +1,13 @@
+package com.example.myshiftapp_new;
+
+public class Employee {
+
+    protected String username;
+    protected String password;
+
+    public Employee(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+}
---
+++ src/main/java/EmployeeMainScreenController.java
@@ -0,0 +1,74 @@
+package com.example.myshiftapp_new;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class EmployeeMainScreenController {
+    @FXML
+    private Button timeDisplay;
+
+    @FXML
+    private Button announcements;
+
+    @FXML
+    private Button account;
+
+    @FXML
+    private Button timeSheet;
+
+    @FXML
+    private Button punchInOut;
+
+    @FXML
+    private Button benefits;
+
+    @FXML
+    private Button notes;
+
+    @FXML
+    private void handleTimeDisplay() throws IOException{
+        Stage stage = (Stage) timeDisplay.getScene().getWindow();
+        MyShiftApplication.switchScene(stage, "TimeDisplay.fxml");
+    }
+
+    @FXML
+    private void handleAnnouncements() throws IOException{
+        Stage stage = (Stage) announcements.getScene().getWindow();
+        MyShiftApplication.switchScene(stage, "Announcements.fxml");
+    }
+
+    @FXML
+    private void handleAccount() throws IOException{
+        Stage stage = (Stage) account.getScene().getWindow();
+        MyShiftApplication.switchScene(stage, "Account.fxml");
+    }
+
+    @FXML
+    private void handleTimeSheetChart() throws IOException{
+        Stage stage = (Stage) timeSheet.getScene().getWindow();
+        MyShiftApplication.switchScene(stage, "TimeSheetChart.fxml");
+    }
+
+    @FXML
+    private void handlePunchInOut() throws IOException{
+        Stage stage = (Stage) punchInOut.getScene().getWindow();
+        MyShiftApplication.switchScene(stage, "PunchInOut.fxml");
+    }
+
+    @FXML
+    private void handleBenefits() throws IOException{
+        Stage stage = (Stage) benefits.getScene().getWindow();
+        MyShiftApplication.switchScene(stage, "Benefits.fxml");
+    }
+
+    @FXML
+    private void handleNotes() throws IOException{
+        Stage stage = (Stage) notes.getScene().getWindow();
+        MyShiftApplication.switchScene(stage, "Notes.fxml");
+    }
+
+
+}
