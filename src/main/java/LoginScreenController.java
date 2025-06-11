53215ba00ae71120f38f01f03b9cc253c268df39
Bhercules1023 <hercb@farmingdale.edu>
2025-06-11 17:25:47 -0400

    Update LoginScreenController.java

--- src/main/java/LoginScreenController.java
+++ src/main/java/LoginScreenController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -19,11 +20,18 @@ public class LoginScreenController {
     private Button loginButton;
 
     @FXML
+    private Label loginFail;
+
+    @FXML
     private void handleLogin() throws IOException {
 
-        String username = usernameField.getText().trim();
-        String password = passwordField.getText().trim();
+        String username = usernameField.getText();
+        String password = passwordField.getText();
 
+        if (username.isEmpty() || password.isEmpty()){
+            loginFail.setText("Please Enter a Username and Password!");
+            return;
+        }
 
         Stage stage = (Stage) loginButton.getScene().getWindow();
         MyShiftApplication.switchScene(stage, "EmployeeMainScreen.fxml");
