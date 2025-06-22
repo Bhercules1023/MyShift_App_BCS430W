6e39f0a6a0e9f4cf94e84981212730911a4ca029
Bhercules1023 <hercb@farmingdale.edu>
2025-06-22 19:52:12 -0400

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,7 @@ module com.example.myshiftapp_new {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-    requires mysql.connector.j;
-
+    requires org.xerial.sqlitejdbc;
 
     opens com.example.myshiftapp_new to javafx.fxml;
     exports com.example.myshiftapp_new;
